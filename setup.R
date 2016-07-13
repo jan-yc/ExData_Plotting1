a86eb86e2f42d445d2c1eaddd9727e7ab8b827ae
jan-yc <jan-yc@users.noreply.github.com>
2016-07-13 20:21:47 +0800

    Added indents for code in comments

--- setup.R
+++ setup.R
@@ -3,6 +3,7 @@ download.file(url, destfile="household_power_consumption.zip"); dldate <- Sys.Da
 save(url, dldate, file = "download.RData")
 
 unzip("household_power_consumption.zip")
+
 # 1 txt file named "power_consumption.txt", 126.8 MB, 2075259 rows, 9 columns
 # Data from 16/12/2006 1724hrs to 26/11/2010 2102hrs, one row per minute
 # We will only be using data from the dates 01/02/2007 and 02/02/2007
@@ -12,11 +13,11 @@ unzip("household_power_consumption.zip")
 # (46*24 + 6) * 60 + 36 = 66636 minutes + 1 header row to skip
 
 # First read the header and store it as a character vector
-# firstrow <- readLines("household_power_consumption.txt", n=1)
-# powernames <- unlist(strsplit(firstrow, ";")); rm(firstrow)
+#     firstrow <- readLines("household_power_consumption.txt", n=1)
+#     powernames <- unlist(strsplit(firstrow, ";")); rm(firstrow)
 # 
 # Read the desired using the following code
-# power <- read.table("household_power_consumption.txt", sep=";", stringsAsFactors = FALSE, 
+#      power <- read.table("household_power_consumption.txt", sep=";", stringsAsFactors = FALSE, 
 #                    col.names = powernames, skip = 66637, nrows = 2880, na.strings = "?")
 #                     
 # Use sum(!complete.cases(power)) to see if NA values are present
